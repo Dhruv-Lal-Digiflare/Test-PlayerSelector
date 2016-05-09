0e7e3d6525c8eec9364a4824f37656a0a65a5949
DhruvLal <dhruvlal@DhruvLal.local>
2016-05-09 13:50:20 -0400

    changed comment 7

--- app/src/main/java/com/digiflare/dhruvlal/playerselector/MainActivity.java
+++ app/src/main/java/com/digiflare/dhruvlal/playerselector/MainActivity.java
@@ -14,7 +14,7 @@ import android.view.MenuItem;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
-//comment 7
+//comment please show
 public class MainActivity extends AppCompatActivity implements PlayerListFragment.OnListInteractionListener {
 
     public static final String[] PLAYERS = { "Stephen Curry", "Derrick Rose", "Kevin Durant", "Kyle Lowry" };
